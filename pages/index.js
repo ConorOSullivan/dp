d255b11e9ef9de379c2320dc640a640788e8a364
ConorOSullivan <cosullivan66@gmail.com>
2023-03-29 09:04:07 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 
-const apiSource = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://arenas-express-heroku.herokuapp.com';
+const apiSource =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://arenas-express-heroku.herokuapp.com';
 
 export default function Home() {
   return (
@@ -11,23 +14,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="container">
-        <div className="bg"></div>
         <div className="image-container">
           <Image
-            src="/outside-manz-invite.jpg"
+            src="/outside-manz-invite.svg"
             layout="fill"
             objectFit="cover"
             quality={100}
           />
-          <div className="cta-container">
-            <div className="links-container">
-              <Link href="https://docs.google.com/forms/d/1XC8lm6gycGh1jGdZ4-0Bql559nuUrDiATMyMqnKfs0I">
-                <a className="cta">RSVP here</a>
-              </Link>
-              <Link href="https://www.instagram.com/reel/CdhUlP6IZ_x/?igshid=YmMyMTA2M2Y=">
-                <a className="cta">Media</a>
-              </Link>
-            </div>
+        </div>
+        <div className="cta-container">
+          <div className="links-container">
+            <Link href="https://docs.google.com/forms/d/1XC8lm6gycGh1jGdZ4-0Bql559nuUrDiATMyMqnKfs0I">
+              <a className="cta">RSVP here</a>
+            </Link>
+            <Link href="https://www.instagram.com/reel/CdhUlP6IZ_x/?igshid=YmMyMTA2M2Y=">
+              <a className="cta">Media</a>
+            </Link>
           </div>
         </div>
         <audio src="/02. Tame Impala - Endors Toi_sample.mp3" autoPlay />
@@ -36,33 +38,32 @@ export default function Home() {
         body {
           margin: 0;
           padding: 0;
-          background-color: rgba(9,14,39,255);
+          background-color: rgba(9, 14, 39, 255);
           height: 100vh;
           overflow: hidden;
         }
         .container {
-          position: relative;
-          height: 100vh;
-        }
-        .bg {
           position: absolute;
           top: 0;
           left: 0;
           right: 0;
           bottom: 0;
-          background-color: rgba(9,14,39,255);
+          background-color: rgba(9, 14, 39, 255);
           z-index: -1;
         }
         .image-container {
           position: relative;
-          height: 100%;
-          padding: 2rem 2rem 2rem calc(2rem + env(safe-area-inset-top));
+          height: calc(100% - env(safe-area-inset-top));
+          padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
         }
         .links-container {
           display: flex;
           flex-direction: row;
           justify-content: space-between;
           align-items: center;
+          width: 100%;
+          max-width: 400px;
+          margin: 0 auto;
         }
         .cta-container {
           position: absolute;
@@ -70,9 +71,9 @@ export default function Home() {
           left: 0;
           right: 0;
           text-align: center;
-          padding: 2rem;
           background-color: rgba(9,14,39,255);
           color: white;
+          padding: 1em 1em calc(1em + env(safe-area-inset-bottom));
         }
         .cta {
           display: inline-block;
@@ -86,9 +87,12 @@ export default function Home() {
         }
         @media only screen and (min-width: 768px) {
           .image-container {
-            max-width: 400px;
+            max-width: 450px;
             margin: 0 auto;
             padding: 10% 5rem;
+          }
+          .cta-container {
+            margin: 0 auto;
           }
         }
       `}</style>
